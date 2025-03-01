66ea0c7d55f75b2425d72cfadb2436e72a3bb848
ifqygazhar <ifqygazhar@gmail.com>
2025-03-02 05:40:00 +0700

    refactor(nvim): added keymap for macos and added for discord share nvim to profile

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,19 +5,38 @@ local map = vim.keymap.set
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 
-map("n", "<n>", "nzzzv")
-map("n", "<N>", "Nzzzv")
+-- For MacOS
+map("n", "n", "nzzzv") -- perbaikan dari <n> menjadi n
+map("n", "N", "Nzzzv") -- perbaikan dari <N> menjadi N
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-vim.keymap.set("n", "<C-_>", function()
+
+-- Comment keymaps for macOS support
+-- macOS menggunakan Command+/ untuk comment
+vim.keymap.set("n", "<M-/>", function()
   require("Comment.api").toggle.linewise.current()
 end, { noremap = true, silent = true })
-vim.keymap.set("x", "<C-_>", function()
+vim.keymap.set("x", "<M-/>", function()
   local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
   vim.api.nvim_feedkeys(esc, "nx", false)
   require("Comment.api").toggle.linewise(vim.fn.visualmode())
 end, { noremap = true, silent = true })
+
+-- For General Operating System
+-- map("n", "<n>", "nzzzv")
+-- map("n", "<N>", "Nzzzv")
+-- -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+-- vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+-- vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+-- vim.keymap.set("n", "<C-_>", function()
+--   require("Comment.api").toggle.linewise.current()
+-- end, { noremap = true, silent = true })
+-- vim.keymap.set("x", "<C-_>", function()
+--   local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+--   vim.api.nvim_feedkeys(esc, "nx", false)
+--   require("Comment.api").toggle.linewise(vim.fn.visualmode())
+-- end, { noremap = true, silent = true })
 
 -- multi select
 vim.g.VM_maps = {
@@ -25,6 +44,11 @@ vim.g.VM_maps = {
   ["Exit"] = "<Esc>",
 }
 vim.g.VM_maps["Complete"] = "<Tab>" -- Gunakan Tab untuk completion
-
 -- undotree
 vim.keymap.set("n", "<leader>uo", require("undotree").open, { noremap = true, silent = true })
+
+-- mapping between screen
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C->", "<C-w>", { noremap = true, silent = true })
